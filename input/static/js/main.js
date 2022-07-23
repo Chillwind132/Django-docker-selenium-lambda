8eed82e379bc8e80936fb9e493345aa3d7c8a582
Mike <mikhail.sidorenko.ca@gmail.com>
2022-07-23 13:02:37 -0700

    fix

--- input/static/js/main.js
+++ input/static/js/main.js
@@ -2,12 +2,14 @@ function waitForElement(){
     if(typeof done_var !== "undefined"){
         window.location.href = "http://52.201.219.127:8000/"; 
         window.open("http://http://52.201.219.127:8000/redirect/", '_blank');
+    }
     else{
         setTimeout(waitForElement, 250);
     }
 }
 
 /*
+
 http://127.0.0.1:8000/
     
 */

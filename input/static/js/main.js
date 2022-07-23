52d2794b2b1fb9b9d16446a3ec37156a274affd2
Mike <mikhail.sidorenko.ca@gmail.com>
2022-07-23 13:15:27 -0700

    +1

--- input/static/js/main.js
+++ input/static/js/main.js
@@ -2,6 +2,7 @@ function waitForElement(){
     if(typeof done_var !== "undefined"){
         window.location.href = "http://52.201.219.127:8000/"; 
         window.open("http://52.201.219.127:8000/redirect", '_blank');
+        console.log("Redirect triggered - log");
     }
     else{
         setTimeout(waitForElement, 250);

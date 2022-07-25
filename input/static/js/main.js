c7a143162dce1c0e2ffdada08a89940c6cd7bef6
Mike <mikhail.sidorenko.ca@gmail.com>
2022-07-24 17:46:20 -0700

    code clean up

--- input/static/js/main.js
+++ input/static/js/main.js
@@ -1,14 +1,10 @@
 function waitForElement(){
     if(typeof done_var !== "undefined"){
-        location.href = "http://3.82.199.161/redirect";
-        setTimeout(() => {  location.href = "http://3.82.199.161"; }, 500);
+        location.href = "/redirect";
+        setTimeout(() => {  location.href = "/"; }, 500);
         console.log("Redirect triggered - log");
     }
     else{
         setTimeout(waitForElement, 250);
     }
 }
-/*
-http://127.0.0.1:8000/
-    
-*/
--- input/views.py
+++ input/views.py
@@ -1,19 +1,18 @@
+from django.views.generic import CreateView
+from .models import Data_sc
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import InputForm
+from django.http import FileResponse
+from django.shortcuts import redirect
 import subprocess
 import os
-from django.views.generic import CreateView
-from .models import Data_sc
 import re
 import boto3
 import mimetypes
-from django.http import FileResponse
-from django.shortcuts import redirect
 import json
 import time
 import zipfile
-
 
 s3_client = boto3.client('s3')
 s3_bucket = "stg-uploaded-screenshots-lambda"
@@ -21,32 +20,26 @@ output_files = "/tmp"
 error_string = "Error"
 
 def index(request):
-    var_name = 'hello'
 
     output = []
     context = {}
     context['form'] = InputForm()
     
-    print(request.POST)
     results = request.POST.getlist('input_url')
     try:
         results_list = results[0].split("\r\n")
-        print(results_list)
         for item in results_list:
             clean_item = item
-            if str(clean_item).strip() == '': #Check if there are any empty entries '', ignore if true
+            if str(clean_item).strip() == '': # Check if there are any empty entries '', ignore if true
                 pass
             else:
-                output.append(item.strip())
-        print (output) #output is a list of final urls to convert
-
+                output.append(item.strip()) # Output is a list of final urls to convert
         connector(output)
-        context['done_flag'] = '1'
+        context['done_flag'] = '1' # Setting flag to trigger the download view
 
     except Exception as e:
-        pass
+        print(e)
     return render(request, "home.html", context)
-
 
 def redirect_view(request):
     response = redirect('/redirect-success/')
@@ -62,8 +55,7 @@ def connector(site_url_list):
         os.chdir("/home/ubuntu/django-project/Django-docker-selenium-lambda/docker-lambda-selenium-backend")
 
         command = "sls invoke --function screenshot_proc --raw --data "  # Space is mandatory
-        #site_url = "https://www.example.com/"
-
+        # item = "https://www.example.com/"
         
         sls_invoke = subprocess.Popen(command + str(item).strip(),
                                         shell=True,
@@ -72,25 +64,22 @@ def connector(site_url_list):
         stdout, stderr = sls_invoke.communicate()
 
         if error_string in str(stdout).strip():
-            print("ERROR STRING DETECTED!!!!!")
+            print("ERROR STRING DETECTED!")
         else:
             url = re.findall('"([^"]*)"', str(stdout).strip())
-            urls.append(url[0])
+            urls.append(url[0]) # Append a list of output files paths to later use for s3 data transfer
         print(stdout)
         print(stderr)
 
     for item in urls:
 
         file_name = output_files + '/' + item
-        s3_client.download_file(s3_bucket,item,file_name)
+        s3_client.download_file(s3_bucket,item,file_name) # Download output files and save to /tmp
 
     ### write urls list to json
-   
     jsonString = json.dumps(urls)
     with open('urls_data.json', 'w') as f:
          json.dump(jsonString, f)
-
-    print("Done")
 
 def download(request):
     data = []
@@ -102,12 +91,13 @@ def download(request):
         data_string = str(d) 
         cleaned_str = data_string.replace("[","").replace("]","").replace('"','').strip()
         data = cleaned_str.split(",")
-    for item in data:
+
+    for item in data: # need to change since no need
 
         upd = tmp + item.strip()
         data_updated.append(upd)
 
-    with zipfile.ZipFile('out.zip', 'w') as zipMe:        
+    with zipfile.ZipFile('out.zip', 'w') as zipMe: # Zip the files before transfer       
         for file_path in data_updated:
 
             name=str(file_path).replace("/tmp/","").strip()
@@ -120,9 +110,9 @@ def download(request):
             mime_type, _ = mimetypes.guess_type('out.zip')
             response = HttpResponse(fh, content_type=mime_type)
             response['Content-Disposition'] = "attachment; filename=out.zip"
-            print("ZIP SENT TO CLIENT!")
-            return response
-     #Return the final zip file to the client
+            print("ZIP SENT TO CLIENT")
+            return response  # Return the final zip file to the client
+
 
 
 
